90decdc369c2dee1ef708f9ed4342f87e6868777
lamkimphu258 <lamkimphu258@gmail.com>
2023-07-06 21:07:19 +0700

    Feature: Add homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,12 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div>
-          <header>
-            <Navbar />
-          </header>
-          <main>{children}</main>
-        </div>
+        <header>
+          <Navbar />
+        </header>
+        <main>{children}</main>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
+import style from "./Welcome.module.css";
+
 export const metadata = {
   title: "Welcome - Task Master",
   description: "Task Master Welcome Page",
 };
 
 export default function Home() {
-  return <div>Main content</div>;
+  return (
+    <div className={style.main}>
+      <div className={style.introduction}>Task Master</div>
+      <div className={style.descripiton}>
+        Task Master is a task management application designed to help
+        individuals and teams efficiently organize, track, and manage their
+        tasks and projects. It provides a centralized platform where users can
+        create, assign, prioritize, and monitor tasks from start to finish.
+      </div>
+    </div>
+  );
 }
